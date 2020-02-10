fe1e31d353556a0c49088d4f1bfb745940b8e89a
Kioto Hirahara <kioto.hirahara@gmail.com>
2020-02-10 03:02:38 +0000

    Add packages to install raspi package script

--- install_raspi_package.sh
+++ install_raspi_package.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 sudo apt install -y emacs ibus-mozc emacs-mozc \
-     vim tmux python3-pip
-
+     vim tmux python3-pip \
+     python3-numpy \
+     python3-pandas \
+     python3-matplotlib
