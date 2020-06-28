c56ffa2a18d2378ea649322188510c055746dd48
Kioto Hirahara <kioto.hirahara@gmail.com>
2020-06-29 06:11:22 +0900

    Add git branch less off setting

--- setup_git.sh
+++ setup_git.sh
@@ -4,3 +4,6 @@ git config --global user.name "Kioto Hirahara"
 git config --global user.email kioto.hirahara@gmail.com
 git config --global credential.helper store
 
+# git branchのlessを無効にする
+git config --global pager.branch false
+
