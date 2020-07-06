4df13570816208df9d0b6c9666917f856dd155be
Kioto Hirahara <kioto.hirahara@gmail.com>
2020-07-06 10:25:19 +0900

    Add git pager option

--- setup_git.sh
+++ setup_git.sh
@@ -5,5 +5,8 @@ git config --global user.email kioto.hirahara@gmail.com
 git config --global credential.helper store
 
 # git branchのlessを無効にする
-git config --global pager.branch false
+#git config --global pager.branch false
+
+# lessで画面クリアしない
+git config --global core.pager "less -R -F -X"
 
