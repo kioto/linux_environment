112c423c4dcf18f60d4db36595a12fe1931eeb0f
Kioto Hirahara <kioto.hirahara@gmail.com>
2021-09-09 11:23:48 +0900

    Add bashrc PS1 setting

--- setup_bash.sh
+++ setup_bash.sh
@@ -3,6 +3,7 @@
 cp .bash_aliases ~/.
 echo export EDITOR=vi >> ~/.bashrc
 echo export PATH=~/.local/bin:\$PATH >> ~/.bashrc
+echo export PS1=\"\\h:\\W \\u\\$ \" >> ~/.bashrc
 
 cp .tmux.conf ~/.
 cp -r .emacs.d ~/.
